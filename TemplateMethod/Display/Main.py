a5ccc7b187bebf830f5c1233c9335ef34f0eb06f
Iwamoto Naoya <august.third.naoya@gmail.com>
2019-10-22 18:31:25 +0900

    [update]refs modify codes in Template Method Display

--- TemplateMethod/Display/Main.py
+++ TemplateMethod/Display/Main.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
-
-
 from CharDisplay import CharDisplay
 from StringDisplay import StringDisplay
 
 
-d1 = CharDisplay('H')
-d2 = StringDisplay('Hello, World.')
-d3 = StringDisplay('こんにちは。')
+def main():
+    d1 = CharDisplay('H')
+    d2 = StringDisplay('Hello, World.')
+    d3 = StringDisplay('こんにちは。')
 
-d1.display()
-d2.display()
-d3.display()
+    d1.display()
+    d2.display()
+    d3.display()
+
+if __name__ == '__main__':
+    main()
