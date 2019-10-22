cd3bcba9d598795c1b9f3fcd8d45592e426218a1
Iwamoto Naoya <august.third.naoya@gmail.com>
2019-10-22 18:43:35 +0900

    [update]refs modify codes in Template Method Access

--- TemplateMethod/Access/Main.py
+++ TemplateMethod/Access/Main.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
-
-
 from TextAccess import TextAccess
 from CsvAccess import CsvAccess
 
 
-a_text = TextAccess()
-a_csv = CsvAccess()
+def main():
+    a_text = TextAccess()
+    a_csv = CsvAccess()
 
-a_text.access()
-a_csv.access()
+    a_text.access()
+    a_csv.access()
+
+if __name__ == '__main__':
+    main()
